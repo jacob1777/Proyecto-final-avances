43c68204308dfcf82b068e24f5f07bd6e0542fef
jacob1777 <80057606+jacob1777@users.noreply.github.com>
2021-04-23 12:03:43 -0500

    Add files via upload

---
+++ FuzzySample1.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FuzzySample1 : MonoBehaviour {
+    
+    
+    
+    public AnimationCurve critical;
+    public AnimationCurve hurt;
+    public AnimationCurve healthy;
+
+    #region UI
+    private const string labelText = "{0} true";
+    public InputField healthInput;
+
+    public Text healthyLabel;
+    public Text hurtLabel;
+    public Text criticalLabel;
+    #endregion
+
+    private float criticalValue = 0f;
+    private float hurtValue = 0f;
+    private float healthyValue = 0f;
+
+    private void Start () {
+        SetLabels();
+    }
+
+    /*
+     * Evaluates all the curves and returns float values
+     */
+    public void EvaluateHealth() {
+        #region UI
+        if (string.IsNullOrEmpty(healthInput.text)) {
+            return;
+        }
+        float inputValue = float.Parse(healthInput.text);
+        #endregion
+        
+        healthyValue = healthy.Evaluate(inputValue);
+        hurtValue = hurt.Evaluate(inputValue);
+        criticalValue = critical.Evaluate(inputValue);
+
+        //Poner las reglas
+
+        SetLabels();
+    }
+
+    /*
+     * Updates the GUI with the evluated values based
+     * on the health percentage entered by the
+     * user.
+     */
+    private void SetLabels() {
+        healthyLabel.text = string.Format(labelText, healthyValue);
+        hurtLabel.text = string.Format(labelText, hurtValue);
+        criticalLabel.text = string.Format(labelText, criticalValue);        
+    }
+}
---
+++ Logicadifusatarea.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logicadifusatarea : MonoBehaviour
+[new Header("Gráfica para evaluar la Salud")]
+    public AnimationCurve muerto;
+    public AnimationCurve herido;
+    public AnimationCurve sano;
+    public AnimationCurve vivo;
+
+    float muetoValue = 0f;
+    float heridoValue = 0f;
+    float sanoValue = 0f;
+    float vivoValue = 0f;
+
+
+    //Variable temporal, se debe poner en el script del agente
+    public int puntosSalud = 100;
+
+    //El metodo evalua los puntos de motivacion del agente
+    public void EvaluarSalud()
+    {
+        muetoValue = muerto.Evaluate(puntosSalud);
+        heridoValue = herido.Evaluate(puntosSalud);
+        sanoValue = sano.Evaluate(puntosSalud);
+        vivoValue = vivo.Evaluate(puntosSalud);
+
+        if(vivoValue > sanoValue)
+        {
+            Debug.Log("estoy intacto");
+        }
+	else if(sanoValue > vivoValue 
+            && heridoValue < sanoValue)
+        {
+            Debug.Log("estoy bien");
+        }
+        else if(muertoValue > heridoMotivadoValue)
+        {
+            Debug.Log("no estoy bien");
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            EvaluarSalud();
+            puntosSalud -= Random.Range(5, 16);
+        }
+    }
+
